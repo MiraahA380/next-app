8494ed9bf2fa2b47924dd153d732252ebb2147ef
Mohamed Miraah <mohamedmiraaha380@gmail.com>
2024-09-30 07:40:04 +0500

    feat: move content below navbar on app-url/

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,9 @@ export default function RootLayout({
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
         <NavBar/>
-        {children}
+        <div className="mt-14">
+            {children}
+        </div>
         </body>
         </html>
     );
