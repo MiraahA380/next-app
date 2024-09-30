e37c2f248cf956a495de2457cea49c6a690d7551
MiraahA380 <mohamedmiraaha380@gmail.com>
2024-09-30 09:47:07 +0500

    authentication.. user registration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,40 @@
-// import NextAuth from "next-auth"
-//
-// // const handler = NextAuth([
-// //
-// // ]);
-//
-// export { handler as GET, handler as POST }
+import NextAuth, {NextAuthOptions} from "next-auth"
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import {prisma} from "@/prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: {label: 'Email', type: 'email', placeholder: 'email'},
+                password: {label: 'Password', type: 'password', placeholder: 'password'}
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.email || !credentials?.password) return null;
+
+                const user = await prisma.user.findUnique({
+                    where: {email: credentials.email}
+                });
+
+                if (!user) return null;
+
+                const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword!);
+
+                return passwordsMatch ? user : null;
+
+            }
+        }),
+    ],
+    session: {
+        strategy: 'jwt'
+    }
+}
+
+const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST}
